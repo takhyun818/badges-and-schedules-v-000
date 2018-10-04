4455609843547a4377f9171b818c5a33ab2f06c8
Tak H Kim <takhyun818@gmail.com>
2018-10-04 17:55:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,11 +18,10 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator.each do |badge|
+  batch_badge_creator(attendees).each do |badge|
     puts badge
   end
   assign_rooms(attendees).each do |assignment|
     puts assignment
   end
-
 end
